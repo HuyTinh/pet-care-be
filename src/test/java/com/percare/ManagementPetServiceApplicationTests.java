f0e1dfd01f32d7c015849ceaef6f5e78d6f6ab13
nguyenmandat08032004@gmail.com <nguyenmandat08032004@gmail.com>
2024-10-21 01:18:21 +0700

    build source code pet management service, have response

---
+++ src/test/java/com/percare/ManagementPetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.percare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementPetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
