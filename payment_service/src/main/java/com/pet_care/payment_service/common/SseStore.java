5ac5e0038ab198f7099367bfa307ed8f63531100
Huy Tinh <170064219+HuyTinh@users.noreply.github.com>
2024-11-03 13:33:40 +0700

    update payment service.

---
+++ payment_service/src/main/java/com/pet_care/payment_service/common/SseStore.java
@@ -0,0 +1,15 @@
+package com.pet_care.payment_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class SseStore {
+    @Bean
+    public static Map<Integer, String> getSseSections(){
+        return new HashMap<Integer, String>();
+    }
+}
