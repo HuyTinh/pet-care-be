e244de4dd59b5a41103174b850f65a3ce18d2c44
Huy Tinh <170064219+HuyTinh@users.noreply.github.com>
2024-12-09 13:51:00 +0700

    update add in service discovery.

---
+++ service_registry/src/test/java/com/pet_care/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet_care.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
